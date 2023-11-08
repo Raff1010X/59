121d7d93e37165be22d6460dfb4951c23aec94c7
Rafał Kowalczyk <kowalczykr@trendglass.pl>
2023-11-08 15:37:58 +0100

    update: components

---
+++ src/components/sliderOpinions/slide/container/container.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import style from './container.module.css';
+
+interface ContainerProps {
+    className?: string;
+    item: {
+        id: number;
+        name: string;
+        role: string;
+        foto: string;
+        logo: string;
+        opinion: string;
+    };
+}
+
+export default function Container(props: ContainerProps) {
+    const { className,  item } = props;
+
+    return (
+        <div className={`${style.wrapper} ${className}`}>
+            <div className={style.header}>
+                <div className={style.foto}>
+                    <Image src={item.foto} alt={item.name} width={100} height={100} />
+                </div>
+                <div className={style.name}>
+                    {item.name}
+                </div>
+                <div className={style.role}>
+                    {item.role}
+                </div>
+            </div>
+            <div className={style.content}>
+                <div className={style.quote}>&quot;</div>
+                <div className={style.opinion}>
+                    {item.opinion}
+                </div>
+            </div>
+            <div className={style.logo}>
+                <Image src={item.logo} alt={item.name} width={100} height={100} />
+            </div>
+        </div>
+    )
+}
---
+++ src/components/sliderOpinions/slide/slide.tsx
@@ -0,0 +1,52 @@
+import Container from './container/container';
+
+import style from './slide.module.css';
+
+interface sliderProps {
+    className?: string;
+    data: {
+        id: number;
+        name: string;
+        role: string;
+        foto: string;
+        logo: string;
+        opinion: string;
+    }[];
+    selected: number;
+    width?: number;
+}
+
+export default function Slide(props: sliderProps) {
+    const { className, data, selected, width } = props;
+
+    return (
+        <div className={`${style.wrapper} ${className}`}>
+            {data.map((item, index) => {
+                return (
+                    <div
+                        key={item.id}
+                        className={`${style.slide} ${index === selected ? style.selectedLeft :
+                            index === selected + 1 ? style.selectedRight :
+                                index === selected - 1 ? style.left :
+                                    index === selected + 2 ? style.right :
+                                        index > selected ? style.deselectedRight :
+                                            style.deselectedLeft
+                            }`}
+                        style={
+                            width === 99.75 && index === selected + 1 ? {
+                                transform: `translateX(200%) rotateY(90deg) scale(0.5)`,
+                                width: `${width}%`,
+                                opacity: 0,
+                            } :
+                                {
+                                    width: `${width}%`,
+                                }
+                        }
+                    >
+                        <Container item={item} />
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
