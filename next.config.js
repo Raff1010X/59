8366d580d7622a37d7102da2229592898a2044c1
Rafał Kowalczyk <kowalczykr@trendglass.pl>
2023-11-10 15:35:49 +0100

    update: components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    // images: {
+    //     loader: 'custom',
+    //     loaderFile: './my/image/loader.js',
+    //   },
+}
 
 module.exports = nextConfig
