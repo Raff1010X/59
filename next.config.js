d5ba2df22f0a6782c6b966b832ee87c3fe42c236
Rafał Kowalczyk <raff1410X@gmail.com>
2023-12-10 18:53:46 +0100

    update: components

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        loader: 'custom',
-        loaderFile: './src/utils/imageLoader.js',
-    },
-    webpack: (config, options) => {
-        config.module.rules.push({
-          test: /\.txt$/,
-          use: 'raw-loader',
-        });
-    
-        return config;
-      },
+  trailingSlash: true,
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: 'custom',
+    loaderFile: './src/utils/imageLoader.js',
+  },
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.txt$/,
+  //     use: 'raw-loader',
+  //   });
+  //   return config;
+  // },
 }
 
 module.exports = nextConfig
